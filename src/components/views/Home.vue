534541cb0226f974fb4daaf3189d26eb5821d9a4
Ednilson Martins <juniorzmartin@gmail.com>
2022-09-08 06:06:57 -0300

    Implementando o componente PesquisarVaga

--- src/components/views/Home.vue
+++ src/components/views/Home.vue
@@ -2,54 +2,20 @@
   <div class="container py-4">
     <div class="row">
       <div class="col">
-        <div class="h-100 p-5 bg-light border rounded-3">
-          <h2>Pesquisar Vagas</h2>
-          <div class="row">
-            <div class="col">
-              <div class="form-group">
-                <label>Titulo da vaga</label>
-                <input type="text" class="form-control" placeholder="Pesquise por palavras chaves, como: 'ANALISTAA', 'Pleno', 'Vue.js'">
-                <small class="form-text text-muted">Informe palavras que estejam relacionadas com o titulo da vaga que você procura</small>
-              </div>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col">
-              <button class="btn btn-outline-dark mt-2" type="button">Buscar</button>
-            </div>
-          </div>
-          <div class="row mt-5">
-            <div class="col-4">
-               <div class="h-100 p-5 rounder-3 border bg-dark text-white">
-                <p>Vagas Abertas</p>
-                <h2>25</h2>
-               </div>
-            </div>
-
-              <div class="col-4">
-                <div class="h-100 p-5 rounder-3 border bg-dark text-white">
-                  <p>Profissionais cadastrados</p>
-                  <h2>125</h2>
-                </div>
-            </div>
-
-              <div class="col-4">
-                <div class="h-100 p-5 rounder-3 border bg-light text-dark">
-                  <p>Visitantes online</p>
-                  <h2>15</h2>
-                </div>
-            </div>
-          </div>
-        </div>
+        <pesquisar-vaga></pesquisar-vaga>
       </div>
     </div>
-    
   </div>
 </template>
 
 <script>
+  import PesquisarVaga from '@/components/comuns/PesquisarVaga.vue'
+
     export default{
         name:'Home',
+        components:{
+          PesquisarVaga
+        },
         created(){
           console.log("criado")
         },
