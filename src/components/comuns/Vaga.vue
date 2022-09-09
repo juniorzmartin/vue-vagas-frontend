43d964f602fea6a775757005dae8dd4d05ff6625
Ednilson Martins <juniorzmartin@gmail.com>
2022-09-09 08:38:55 -0300

    Criando o componente Vaga

---
+++ src/components/comuns/Vaga.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="row mt-5">
+    <div class="col">
+      <div class="card">
+        <div class="card-header bg-dark text-white">Título da vaga</div>
+        <div class="card-body">
+          <p>Descrição truncada da vaga...</p>
+        </div>
+        <div class="card-footer">
+          <small class="text-muted">Salário: R$ 6.000,00 | Modalidade: Home Office | Tipo: CLT | Publicação: 09/09/2022</small>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default ({
+    name: 'Vaga'
+})
+</script>
--- src/components/views/Home.vue
+++ src/components/views/Home.vue
@@ -5,6 +5,20 @@
         <pesquisar-vaga></pesquisar-vaga>
       </div>
     </div>
+
+  <div class="row mt-5">
+    <div class="col">
+      <vaga />
+    </div>
+  </div>
+
+   <div class="row mt-5">
+    <div class="col">
+      <vaga />
+    </div>
+  </div>
+
+
   <div class="row mt-5">
     <div class="col-4">
       <indicador titulo="Vagas abertas" indicador="100" bg="bg-dark" color="text-white"></indicador>
@@ -24,11 +38,13 @@
 <script>
   import PesquisarVaga from '@/components/comuns/PesquisarVaga.vue'
   import Indicador from '@/components/comuns/Indicador.vue'
+  import Vaga from '@/components/comuns/Vaga.vue'
     export default{
         name:'Home',
         components:{
           PesquisarVaga,
-          Indicador
+          Indicador,
+          Vaga
         },
         data: () => ({
           usuarioOnline : 0
