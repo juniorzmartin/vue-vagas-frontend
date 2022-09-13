a3cf69bc5324588c259410ff44881ab7362d7ded
Ednilson Martins <juniorzmartin@gmail.com>
2022-09-13 09:50:18 -0300

    Exibindo vagas registradas em LocalStorage

--- src/components/views/Home.vue
+++ src/components/views/Home.vue
@@ -42,48 +42,7 @@
         },
         data: () => ({
           usuarioOnline : 0,
-          vagas: [
-            {
-              titulo: 'Analista Programador PHP Pleno',
-              descricao: 'Profissional com conhecimentos em PHP, Laravel e MySQL. Necessário 3 anos de experiências. Atuará na manutenção de sistemas legados da empresa.',
-              salario: 6000,
-              modalidade: 'Home Office',
-              tipo: 'PJ',
-              publicacao: '2022-10-10'
-            },
-            {
-              titulo: 'Programador JavaScript Angular',
-              descricao: 'Profissional com conhecimentos avançados em JavaScript e Angular.',
-              salario: 5000,
-              modalidade: 'Presencial',
-              tipo: 'CLT',
-              publicacao: '2022-03-1'
-            },
-            {
-              titulo: 'Programador JavaScript Vue',
-              descricao: 'Profissional com conhecimentos avançados em JavaScript e Vue.',
-              salario: 5000,
-              modalidade: 'Home Office',
-              tipo: 'CLT',
-              publicacao: '2022-10-22'
-            },
-            {
-              titulo: 'Analista de Banco de Dados Sênior',
-              descricao: 'Domínio dos bancos de dados SQL Server, Oracle, Postgre e MySQL',
-              salario: 9000,
-              modalidade: 'Presencial',
-              tipo: 'PJ',
-              publicacao: '2022-10-06'
-            },
-            {
-              titulo: 'Programador Web Júnior',
-              descricao: 'Conhecimentos básicos em HTML, CSS, JavaScript, Bootstrap, PHP e MySQL',
-              salario: 3000,
-              modalidade: 'Presencial',
-              tipo: 'CLT',
-              publicacao: '2022-12-05'
-            }
-          ]
+          vagas: []
         }),
         methods: {
           getUsuarioOnline(){
@@ -93,6 +52,11 @@
 
         created(){
           setInterval(this.getUsuarioOnline, 1000)
+        },
+
+        //mounted(){
+          activated(){
+          this.vagas = JSON.parse(localStorage.getItem('vagas'))
         }
     }
 </script>
